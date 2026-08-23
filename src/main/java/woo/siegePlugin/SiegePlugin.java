522c1b92d5b357380b2b772c30c60c374a79aa52
Eric Wei <ewei123@terpmail.umd.edu>
2026-08-23 02:03:31 -0400

    added git ignore

--- src/main/java/woo/siegePlugin/SiegePlugin.java
+++ src/main/java/woo/siegePlugin/SiegePlugin.java
@@ -7,6 +7,7 @@ public final class SiegePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("SiegePlugin enabled");
     }
 
     @Override
