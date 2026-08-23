5ee91c761df06953e0639c78d81a23182fedfe71
Eric Wei <ewei123@terpmail.umd.edu>
2026-08-23 01:20:44 -0400

    initial project scaffold

---
+++ src/main/java/woo/siegePlugin/SiegePluginLoader.java
@@ -0,0 +1,12 @@
+package woo.siegePlugin;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+
+class SiegePluginLoader implements PluginLoader {
+
+    @Override
+    public void classloader(final PluginClasspathBuilder builder) {
+        // Add dynamically loaded libraries here
+    }
+}
